40f750c9500e6e01cedce0baf5c65e8802335e7b
Rauno Tegelmann <raunoot@gmail.com>
2017-06-12 10:16:36 +0300

    various navbar tweaks, including collapsing to burger menu for mobile displays

--- page/pvp.php
+++ page/pvp.php
@@ -42,16 +42,28 @@
 	<nav class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container-fluid">
 	    	<div class="navbar-header">
-	      		<a class="navbar-brand" href="#">Monster Builder</a>
+	    		<!-- Burgermenüü -->
+	    		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+				</button>
+	      		<a class="navbar-brand" href="main.php">Monster Builder</a>
 	    	</div>
-		    <ul class="nav navbar-nav">
-		      	<li><a href="main.php">Home</a></li>
-		      	<li class="active"><a href="pvp.php">PvP</a></li>
-		      	<li><a href="monster.php">PvAI</a></li>
-		    </ul>
-		    <ul class="nav navbar-nav navbar-right">
-		    	<li><a href="?logout=1">Logout</a></li>
-		    </ul>
+	    	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			    <ul class="nav navbar-nav">
+			      	<li class="active"><a href="pvp.php">Singleplayer</a></li>
+			      	<li><a href="monster.php">Multiplayer</a></li>
+			    </ul>
+			    <ul class="nav navbar-nav navbar-right">
+			    	<li>
+						<a href="?logout=1">
+							<span class="glyphicon glyphicon-log-out"></span>
+						</a>
+			    	</li>
+			    </ul>
+			</div>
 	  	</div>
 	</nav>
 
@@ -95,5 +107,6 @@
 			</div><!--interface-->
 
 		</div><!--container-->
-	</body>
+
+</body>
 </html>
